33f84a8cb8f5884055be971a5461629fd25ecc66
gabigam <78092230+gabigam@users.noreply.github.com>
2024-10-03 14:12:06 -0300

    CadastroPage.tsx

---
+++ CadastroPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import CadastroGestorSetorial from './CadastroGestorSetorial';
+
+const CadastroGestorSetorialPage: React.FC = () => {
+  return (
+    <div>
+      <CadastroGestorSetorial />
+    </div>
+  );
+};
+
+export default CadastroGestorSetorialPage;
